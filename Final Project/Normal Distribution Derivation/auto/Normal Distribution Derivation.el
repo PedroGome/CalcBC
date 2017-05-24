be3adab32c45b70623f56e0cf8e8caa9decac3f1
ZenTauro <pedrogomezmartin1999@gmail.com>
2017-05-24 12:38:45 -0400

    Final Project Part 1 Section 1

---
+++ Final Project/Normal Distribution Derivation/auto/Normal Distribution Derivation.el
@@ -0,0 +1,20 @@
+(TeX-add-style-hook
+ "Normal Distribution Derivation"
+ (lambda ()
+   (TeX-add-to-alist 'LaTeX-provided-package-options
+                     '(("inputenc" "utf8") ("babel" "american") ("geometry" "letterpaper" "left=3cm" "right=2cm" "top=2.5cm" "bottom=1.5cm")))
+   (TeX-run-style-hooks
+    "latex2e"
+    "article"
+    "art10"
+    "mathtools"
+    "multicol"
+    "setspace"
+    "inputenc"
+    "fancyhdr"
+    "tabularx"
+    "babel"
+    "enumerate"
+    "geometry"))
+ :latex)
+
